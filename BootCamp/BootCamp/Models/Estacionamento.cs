f20621a50cfc2492005e8591f0bf70d84012e3fb
Fábio Henrique Trevezane <56356783+fabio-sudo@users.noreply.github.com>
2025-05-19 18:49:45 -0300

    Atualizacao

---
+++ BootCamp/BootCamp/Models/Estacionamento.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp.Models
+{
+    public class Estacionamento
+    {
+        private List<string> veiculos = new List<string>();
+
+        public void AdicionarVeiculo()
+        {
+            Console.Write("Digite a placa do veículo para estacionar: ");
+            string placa = Console.ReadLine();
+            veiculos.Add(placa);
+            Console.WriteLine("Veículo cadastrado com sucesso!");
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.Write("Digite a placa do veículo para remover: ");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Contains(placa))
+            {
+                veiculos.Remove(placa);
+                Console.WriteLine("Veículo removido com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Veículo não encontrado.");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Count == 0)
+            {
+                Console.WriteLine("Nenhum veículo estacionado.");
+                return;
+            }
+
+            Console.WriteLine("Os veículos estacionados são:");
+            foreach (var v in veiculos)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
---
+++ BootCamp/BootCamp/Program.cs
@@ -0,0 +1,39 @@
+﻿using BootCamp.Models;
+
+Estacionamento estacionamento = new Estacionamento();
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            estacionamento.AdicionarVeiculo();
+            break;
+
+        case "2":
+            estacionamento.RemoverVeiculo();
+            break;
+
+        case "3":
+            estacionamento.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+    Console.ReadLine();
+}
